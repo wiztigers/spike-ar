571ffe337f20e81ff77c92c931436e60d6651f2f
wiztigers <regis.witz@free.fr>
2017-02-21 13:32:26 +0100

    camera feed appeared rotated by 90 degrees on mobile ; fix it

--- Assets/scripts/PlayCamera.cs
+++ Assets/scripts/PlayCamera.cs
@@ -3,11 +3,18 @@
 public class PlayCamera: MonoBehaviour {
 
 	private WebCamTexture cammy;
+	private Quaternion rotation;
 
 	void Start() {
 		cammy = new WebCamTexture();
-		gameObject.GetComponent<MeshRenderer>().material.mainTexture = cammy;
+		GetComponent<Renderer>().material.mainTexture = cammy;
 		cammy.Play();
+		rotation = transform.rotation;
+	}
+
+	void Update() {
+		// https://forum.unity3d.com/threads/webcamtexture-flipping-and-rotating-90-degree-ios-and-android.143856/
+		transform.rotation = rotation * Quaternion.AngleAxis(cammy.videoRotationAngle, Vector3.back);
 	}
 
 	void OnGUI() {
