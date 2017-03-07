d565eb3599fd4485b7021d83fb12a070b1006a55
wiztigers <regis.witz@free.fr>
2017-03-07 12:13:14 +0100

    moar tests

---
+++ Assets/scripts/util/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class Extensions {
+
+	public static void AddRange<T>(this ICollection<T> c, IEnumerable<T> e) {
+		foreach (var item in e) c.Add(item);
+	}
+}
